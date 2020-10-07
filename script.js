213464fb6d71093fe6d4948efc3852bfffdebca9
Zena Vilenia <33894812+zenavilenia@users.noreply.github.com>
2020-10-07 19:52:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const form = document.querySelector("form");
 const input = document.getElementById("item");
 const ul = document.querySelector("ul");
-let items = [];
+// let items = [];
 
-if (localStorage.getItem('items')) {
-    items = JSON.parse(localStorage.getItem('items'))
-}
+// if (localStorage.getItem('items') !== undefined) {
+//     items = JSON.parse(localStorage.getItem('items'))
+// } else {
+
+
+// ternary if
+let items = (localStorage.getItem('items')) ? JSON.parse(localStorage.getItem('items')) : []
 
 form.addEventListener('submit', function (e) {
     items.push(input.value);
@@ -13,11 +17,23 @@ form.addEventListener('submit', function (e) {
     localStorage.setItem('items', JSON.stringify(items));
 })
 
-for (var i = 0; i < items.length; i++) {
-    console.log(items[i])
-
-    const li = document.createElement("li");
-    li.textContent = items[i];
-
-    ul.appendChild(li);
+function showitems() {
+    for (var i = 0; i < items.length; i++) {
+        console.log(items[i])
+    
+        const li = document.createElement("li");
+        li.textContent = items[i];
+    
+        ul.appendChild(li);
+    }
 }
+
+function clearItems() {
+    localStorage.removeItem('items')
+
+    items = [];
+
+    ul.innerHTML = '';
+}
+
+showitems()
